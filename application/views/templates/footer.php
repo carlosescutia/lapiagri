0af25a3f26af55c0bc3fd8f7ca56e72e5aadb39b
Carlos Escutia <carlosescutia@gmail.com>
2023-12-09 19:55:14 -0600

    Se agrega información de desarrollador.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,29 +4,45 @@
         </div>
 
         <footer class="footer">
-            <ul class="nav justify-content-center">
-                <li class="nav-item">&copy; <?= $anio_org_sitio ?? '2023' ?> <?= $nom_org_sitio ?? 'Organización' ?></li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">·</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item"><i class="bi bi-telephone"></i> <?= $tel_org_sitio ?? '(123) 456 7890' ?> &nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">·</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item">&nbsp;</li>
-                <li class="nav-item"><i class="bi bi-envelope"></i> <?= $correo_org_sitio ?? 'contacto@organizacion.inet' ?> &nbsp; </li>
-            </ul>
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-6 offset-sm-3">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">&copy; <?= $anio_org_sitio ?? '2023' ?> <?= $nom_org_sitio ?? 'Organización' ?></li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">·</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item"><i class="bi bi-telephone"></i> <?= $tel_org_sitio ?? '(123) 456 7890' ?> &nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">·</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item">&nbsp;</li>
+                        <li class="nav-item"><i class="bi bi-envelope"></i> <?= $correo_org_sitio ?? 'contacto@organizacion.inet' ?> &nbsp; </li>
+                    </ul>
+                </div>
+                <div class="col-sm-2 offset-sm-1">
+                    <ul class="list-unstyled">
+                        <a href="#">
+                            <li class="nav-item small">
+                                Desarrollado por
+                                <span><img src="<?=base_url()?>img/cenit.svg" class="imagen_ancho" alt="cenit"></span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
         </footer>
         
     </body>
